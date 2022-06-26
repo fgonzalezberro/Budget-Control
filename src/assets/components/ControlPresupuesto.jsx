323e30128c9dac2fdffcece25b9720bf41bed52b
Developer <fgonzalezberro@gmail.com>
2022-06-25 23:59:24 -0300

    Format price

--- src/assets/components/ControlPresupuesto.jsx
+++ src/assets/components/ControlPresupuesto.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const ControlPresupuesto = ({presupuestoInicial}) => {
+
+    // Format price
+    const formatPrice = (cant) =>{
+     return cant.toLocaleString("en-US" , {
+        style: "currency",
+        currency: "USD"
+     });
+    }
+
     return (
         <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
             <div>Gráfica aquí</div>
 
             <div className='contenido-presupuesto'>
                 <p>
-                    <span>Presupuesto: </span> ${presupuestoInicial}
+                    <span>Presupuesto: </span> {formatPrice(presupuestoInicial)}
                 </p>
             </div>
         </div>
