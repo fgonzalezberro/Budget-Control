14cc6736f9a79e8cab14a81f25b53f148a16c32e
Developer <fgonzalezberro@gmail.com>
2022-06-25 23:16:16 -0300

    Validate budget form

--- src/assets/components/NuevoPresupuesto.jsx
+++ src/assets/components/NuevoPresupuesto.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+// Import react hooks
+import {useState} from 'react';
+
+// Import react components
+import Mensaje from './Mensaje';
 
 const NuevoPresupuesto = ({presupuestoInicial , setPresupuestoInicial}) => {
+
+    const [mensaje , setMensaje ] = useState("");
+
+    // Validate form data
+    const handlePresupuesto = (e) =>{
+        e.preventDefault();
+
+        presupuestoInicial < 0 || !Number(presupuestoInicial) ? setMensaje("Dato ingresado no valido") :
+                                                                setMensaje("Succcess");
+    }
+
     return (
         <div className="contenedor-presupuesto contenedor sombra">
-            <form className='formulario'>
+            <form onSubmit={handlePresupuesto} className='formulario'>
                 <div className='campo'>
                     <label>Definir presupuesto</label>
 
@@ -19,6 +34,8 @@ const NuevoPresupuesto = ({presupuestoInicial , setPresupuestoInicial}) => {
                     type="submit" 
                     value="Añadir"
                 />
+
+                {mensaje && <Mensaje tipo={"error"}>{mensaje}</Mensaje>}
             </form>
         </div>
     );
