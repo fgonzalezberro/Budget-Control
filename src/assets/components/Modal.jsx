2db6c422421a6f82d4558c21ede9d548aa8c50a4
Developer <fgonzalezberro@gmail.com>
2022-06-26 23:01:45 -0300

    Adding expense form

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -26,6 +26,49 @@ const Modal = ({setModal, animacionModal , setAnimacionModal}) => {
 
             <form className={`formulario ${animacionModal ? "animar" : "cerrar"}`}>
                 <legend>Nuevo gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre gasto</label>
+
+                    <input 
+                        id="nombre"
+                        type="text" 
+                        placeholder="Añade nombre del gasto"
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="cantidad">Cantidad</label>
+
+                    <input 
+                        id="cantidad"
+                        type="text" 
+                        placeholder="Añade cantidad. Ej: 300"
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Categoria</label>
+
+                    <select 
+                        id="categoria"
+                    >
+                        
+                        <option value="">-- Seleccione categoria</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+
+                <input 
+                    type="submit" 
+                    value="Añadir gasto"
+                />
             </form>
         </div>
     );
